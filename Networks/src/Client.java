3024111e3142abee56b97c0dd42cd1ba15372a3c
ali massoud <ali.masoud@lau.edu>
2020-12-03 21:51:24 +0200

    Ahmad & Rayan version

--- Networks/src/Client.java
+++ Networks/src/Client.java
@@ -1,21 +1,30 @@
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.util.Scanner;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class Client {
-	public static byte[] serialize(OurPacket obj) throws IOException{
+	public static byte[] serialize(OurPacket obj) throws IOException {
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 		ObjectOutputStream os = new ObjectOutputStream(out);
 		os.writeObject(obj);
-		byte [] dt = out.toByteArray();
+		byte[] dt = out.toByteArray();
 		out.close();
 		os.close();
 		return dt;
-		
-	
 	}
-	
-	public static Object deserialize(byte []data) throws IOException, ClassNotFoundException {
+
+	public static Object deserialize(byte[] data) throws IOException, ClassNotFoundException {
 		ByteArrayInputStream in = new ByteArrayInputStream(data);
 		ObjectInputStream is = new ObjectInputStream(in);
 		Object b = is.readObject();
@@ -23,34 +32,83 @@ public class Client {
 		in.close();
 		return b;
 	}
+
+	public static final int MAX_PACKET_DATA_SIZE = 512;
+
 	public static void main(String[] args) throws IOException, ClassNotFoundException {
 		// TODO Auto-generated method stub
 		BufferedReader infromusr = new BufferedReader(new InputStreamReader(System.in));
 		DatagramSocket clientsckt = new DatagramSocket();
-		
+
 		InetAddress IPaddress = InetAddress.getByName("localhost");
-		
+
 		byte[] sendata = new byte[1024];
-		byte[] rcvdata = new byte [1024];
+		byte[] rcvdata = new byte[1024];
 		String data = "";
 		File inputFile = new File("test.txt");
 		Scanner sc = new Scanner(inputFile);
-		while(sc.hasNextLine()) {
-			data += sc.nextLine();
+		String temp = "";
+//		int seqNum = 0;
+		int ack = 0, linepointer = 0;
+
+		boolean fileIsSent = false;
+
+		while (!fileIsSent) {
+
+			//This is for fragmentation of the file to be sent
+			boolean Full = false;
+			while (!Full) {
+				while (linepointer < temp.length()) {
+					data += temp.charAt(linepointer);
+					
+					if (data.getBytes().length == MAX_PACKET_DATA_SIZE) {
+						Full = true;
+						break;
+					}
+					linepointer++;
+				}
+				if (linepointer == temp.length()) {
+					linepointer = 0;
+					if(sc.hasNext())
+						temp = sc.nextLine();
+					else Full = true;
+				}
+			}
+			//Fragmentation ends
+			
+			//Sending -- no changes
+			OurPacket packet1 = new OurPacket(6789, 6789, 1024, ack, "1011101110", 0, data);
+			sendata = serialize(packet1);
+			DatagramPacket sendpckt = new DatagramPacket(sendata, sendata.length, IPaddress, 6789);
+
+			try {
+				clientsckt.send(sendpckt);
+				clientsckt.setSoTimeout(5000);
+				DatagramPacket rcvpckt = new DatagramPacket(rcvdata, rcvdata.length);
+				clientsckt.receive(rcvpckt);
+
+				OurPacket pak_rcv = (OurPacket) deserialize(rcvpckt.getData());
+				System.out.println("From Server " + pak_rcv.toString());
+
+				while (ack != pak_rcv.acknowledgeBits) {// check seqNum in phase four -- checksum for phase 3
+					sendpckt = new DatagramPacket(sendata, sendata.length, IPaddress, 6789);
+
+					clientsckt.send(sendpckt);
+					clientsckt.setSoTimeout(1000);
+					rcvpckt = new DatagramPacket(rcvdata, rcvdata.length);
+					clientsckt.receive(rcvpckt);
+					pak_rcv = (OurPacket) deserialize(rcvpckt.getData());
+				}
+//				seqNum++;
+				ack = (ack == 0) ? 1 : 0;
+//				if(seqNum-1 == PacketsReuired)
+//					fileIsSent =true;
+			} catch (Exception e) {
+				System.out.println("Timeout exceeded");
+			}
 		}
-		OurPacket send_packet_class = new OurPacket(6789,6789,1024,5,"1011101110",0,data);
-		sendata = serialize(send_packet_class);
-		
-		DatagramPacket sendpckt = new DatagramPacket(sendata,sendata.length,IPaddress,6789);
-		
-		clientsckt.send(sendpckt);
-		
-		DatagramPacket rcvpckt = new DatagramPacket (rcvdata,rcvdata.length);
-		clientsckt.receive(rcvpckt);
-		
-		OurPacket rcv_packet_class = (OurPacket)deserialize(rcvpckt.getData());
-		System.out.println("From Server "+rcv_packet_class.toString());
 		clientsckt.close();
+
 	}
 
 }
--- Networks/src/OurPacket.java
+++ Networks/src/OurPacket.java
@@ -1,15 +1,15 @@
 
-public class OurPacket implements java.io.Serializable{
+public class OurPacket implements java.io.Serializable {
 	int sourcePort, destinationPort, lenghOfData, acknowledgeBits, seqNum;
 	final String sourceIP = "";
-	final String destinationIP= "";
+	final String destinationIP = "";
 	String checksum;
 	String data;
-	
-	
+	boolean ack = false;
 	public int getSourcePort() {
 		return sourcePort;
 	}
+
 	public OurPacket() {
 		sourcePort = 0;
 		destinationPort = 0;
@@ -19,8 +19,9 @@ public class OurPacket implements java.io.Serializable{
 		acknowledgeBits = -1;
 		data = "Test packet";
 	}
-	public OurPacket(int sourcePort, int destinationPort, int lengthOfData,
-			int seqNum, String checkSum,int acknowledgeBits, String data){
+	
+	public OurPacket(int sourcePort, int destinationPort, int lengthOfData, int seqNum, String checkSum,
+			int acknowledgeBits, String data) {
 		setAcknowledgeBits(acknowledgeBits);
 		setChecksum(checkSum);
 		setData(data);
@@ -29,52 +30,64 @@ public class OurPacket implements java.io.Serializable{
 		setSourcePort(sourcePort);
 		setLenghOfData(lengthOfData);
 	}
+
 	public void setSourcePort(int sourcePort) {
-			this.sourcePort = sourcePort;
+		this.sourcePort = sourcePort;
 	}
+
 	public int getDestinationPort() {
 		return destinationPort;
 	}
+
 	public void setDestinationPort(int destinationPort) {
-			this.destinationPort = destinationPort;
+		this.destinationPort = destinationPort;
 	}
+
 	public int getLenghOfData() {
 		return lenghOfData;
 	}
+
 	public void setLenghOfData(int lenghOfData) {
 		this.lenghOfData = lenghOfData;
 	}
+
 	public int getSeqNum() {
 		return seqNum;
 	}
+
 	public void setSeqNum(int seqNum) {
 		this.seqNum = seqNum;
 	}
+
 	public String getChecksum() {
 		return checksum;
 	}
+
 	public void setChecksum(String checksum) {
 		this.checksum = checksum;
 	}
+
 	public int getAcknowledgeBits() {
 		return acknowledgeBits;
 	}
+
 	public void setAcknowledgeBits(int acknowledgeBits) {
 		this.acknowledgeBits = acknowledgeBits;
 	}
+
 	public String getData() {
 		return data;
 	}
+
 	public void setData(String data) {
 		this.data = data;
 	}
+
 	@Override
 	public String toString() {
 		return "OurPacket [sourcePort=" + sourcePort + ", destinationPort=" + destinationPort + ", lenghOfData="
 				+ lenghOfData + ", acknowledgeBits=" + acknowledgeBits + ", seqNum=" + seqNum + ", sourceIP=" + sourceIP
 				+ ", destinationIP=" + destinationIP + ", checksum=" + checksum + ", data=" + data + "]";
 	}
-	
-	
-	
+
 }
